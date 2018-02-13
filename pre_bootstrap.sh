432a8fafe32c2078dd94d1977cb6e51c067c881f
Charles Reid <charlesreid1@gmail.com>
2018-02-13 12:12:17 -0800

    make vim directories in pre-bootstrap script

--- pre_bootstrap.sh
+++ pre_bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Actually make the swap directory vim is going to use
+mkdir -p ~/.vim/swap
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/backups
+
 # Change shell to bash
 BASH="/bin/bash"
 echo "About to set shell to ${BASH}"
