9e0e659d5c7e57080bca7b655bbd636d5a1fcdf9
Charles Reid <charlesreid1@gmail.com>
2018-02-13 22:34:12 -0800

    removing shell script

--- scripts/get_trimmed_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Runs a series of wget commands to download data from OSF
-
-echo "Running the following commands:"
-cat trimmed_data.dat | awk '{ print "wget "$2" -O "$1;}' 
-
-eval `cat trimmed_data.dat | awk '{ print "wget "$2" -O "$1;}'`
