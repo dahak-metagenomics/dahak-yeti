a96334d3ab93bd3a6f81587e69ffc6a12b71c333
Charles Reid <charlesreid1@gmail.com>
2018-02-19 11:57:13 -0800

    Removing old Bash versions of scripts replaced by new Python versions

--- scripts/prepare_biocontainers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull quay.io/biocontainers/sourmash:2.0.0a3--py36_0
-docker pull quay.io/biocontainers/krona:2.7--pl5.22.0_1
-docker pull quay.io/biocontainers/kaiju:1.5.0--pl5.22.0_0
-
--- scripts/unpack_kaiju.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir kaijudb
-cd kaijudb
-curl -LO http://kaiju.binf.ku.dk/database/kaiju_index_nr_euk.tgz
-tar zxvf kaiju_index_nr_euk.tgz
-rm -r kaiju_index_nr_euk.tgz
-cd ../
