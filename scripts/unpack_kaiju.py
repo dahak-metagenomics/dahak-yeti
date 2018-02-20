6098ad22225ca3cce572ab2c2aa445d612d1609a
Chaz Reid <charlesreid1@Gmail.com>
2018-02-20 15:12:44 -0800

    Update unpack_kaiju.py

--- scripts/unpack_kaiju.py
+++ scripts/unpack_kaiju.py
@@ -7,8 +7,8 @@ kaijudir = pwd+"/kaijudirb"
 tarfile = "kaiju_index_nr_euk.tgz"
 url = "http://kaiju.binf.ku.dk/database/kaiju_index_nr_euk.tgz"
 
-subprocess.call(["mkdir",kaijudir])
-subprocess.call(["curl","-LO",url], cwd=kaijudir)
-subprocess.call(["tar","zxvf",tarfile], cwd=kaijudir)
-subprocess.call(["rm","-f",tarfile], cwd=kaijudir)
+subprocess.call(["mkdir",kaijudir],    cwd=pwd)
+subprocess.call(["curl","-LO",url],    cwd=kaijudir)
+subprocess.call(["tar","zxvf",tarfile],cwd=kaijudir)
+subprocess.call(["rm","-f",tarfile],   cwd=kaijudir)
 
