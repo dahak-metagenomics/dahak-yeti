4f8f52d727c09118e7b80f576b3d99a094f11f61
Charles Reid <charlesreid1@gmail.com>
2018-02-20 14:24:06 -0800

    update run all part 2 to use python scripts

--- scripts/run_all_part2.sh
+++ scripts/run_all_part2.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-./prepare_biocontainers.sh
+./prepare_biocontainers.py
 
-./get_sbt.sh
+./get_sbt.py
 
 ./get_trimmed_data.py
 
-./calculate_signatures.sh
+./calculate_signatures.py
 
 ## Skipping this script
 #./compare_components.sh
 
-./unpack_kaiju.sh
+./unpack_kaiju.py
 
-## Start from here...
-#./run_kaiju.sh
-#
-#./kaiju_to_korona.sh
-#
-#./filter_taxa.sh
-#
+./run_kaiju.py
+
+./kaiju2krona.py
+
+./filter_taxa.sh
+
 #./visualize_korona.sh
 
