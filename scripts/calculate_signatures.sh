8dea9dd8dc60b3fe9840f7635feaa7d2a055d96b
Charles Reid <charlesreid1@gmail.com>
2018-02-20 18:55:13 -0800

    Fix command line args

--- scripts/calculate_signatures.sh
+++ scripts/calculate_signatures.sh
@@ -19,7 +19,8 @@ do
         docker run -v ${PWD}:/data quay.io/biocontainers/sourmash:2.0.0a3--py36_0 sourmash compute \
                 --merge /data/${base}.trim2.fq.gz \
                 --track-abundance \
-                --scaled 10000 -k 21,31,51 \
+                --scaled 10000 \
+                -k 21,31,51 \
                 /data/${base}_1.trim2.fq.gz \
                 /data/${base}_2.trim2.fq.gz \
                 -o /data/${base}${sigsuffix}
@@ -43,7 +44,8 @@ do
         docker run -v ${PWD}:/data quay.io/biocontainers/sourmash:2.0.0a3--py36_0 sourmash compute \
             --merge /data/${base}.trim30.fq.gz \
             --track-abundance \
-            --scaled 10000 -k 21,31,51 \
+            --scaled 10000 \
+            -k 21,31,51 \
             /data/${base}_1.trim30.fq.gz \
             /data/${base}_2.trim30.fq.gz \
             -o /data/${base}${sigsuffix}
