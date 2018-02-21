7dd9265f5ecdb6fed775af208f0f581356e468e3
Charles Reid <charlesreid1@gmail.com>
2018-02-20 17:00:24 -0800

    Added 1 percent classified docker run command

--- scripts/filter_taxa.py
+++ scripts/filter_taxa.py
@@ -32,6 +32,7 @@ def filter_taxa():
             # filtering taxa
 
             reportoutfile = prefix + ".kaiju_out_krona.1percenttotal.summary"
+            classifiedreportoutfile = prefix + ".kaiju_out_krona.1percentclassified.summary"
 
             if(os.path.isfile(reportoutfile)):
                 print("Skipping file %s, file exists."%(reportoutfile))
@@ -49,6 +50,26 @@ def filter_taxa():
                 cmd += ["-i","%s/%s"%(datadir, filename)]
                 cmd += ["-r","genus"]
                 cmd += ["-m","1"]
+                cmd += ["-o","%s/%s"%(datadir, reportoutfile)]
+
+                print("Running this docker command from dir %s:"%(pwd))
+                print(" ".join(cmd))
+                print()
+
+                subprocess.call(cmd, cwd=pwd)
+
+
+                cmd = ["docker","run"]
+                cmd += ["-v","%s:%s"%(pwd,datadir)]
+                cmd += ["quay.io/biocontainers/kaiju:1.6.1--pl5.22.0_0"]
+                cmd += ["kaijuReport"]
+                cmd += ["-v"]
+                cmd += ["-t","%s/%s/nodes.dmp"%(datadir,kaijudirname)]
+                cmd += ["-n","%s/%s/names.dmp"%(datadir,kaijudirname)]
+                cmd += ["-i","%s/%s"%(datadir, filename)]
+                cmd += ["-r","genus"]
+                cmd += ["-m","1"]
+                cmd += ["-u"]
                 cmd += ["-o","%s/%s"%(datadir, reportoutfile)]
 
                 print("Running this docker command from dir %s:"%(pwd))
