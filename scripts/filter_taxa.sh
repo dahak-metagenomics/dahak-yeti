f19056b910e97a5c67a7f5b65f6226026b05a962
Charles Reid <charlesreid1@gmail.com>
2018-02-20 17:00:51 -0800

    Bump kaiju version, fix indentation

--- scripts/filter_taxa.sh
+++ scripts/filter_taxa.sh
@@ -4,30 +4,30 @@
 
 for i in *trim{"2","30"}.out
 do
-        docker run -v /home/ubuntu/data:/data quay.io/biocontainers/kaiju:1.5.0--pl5.22.0_0 kaijuReport 
-    -v \
-    -t \
-        /data/kaijudb/nodes.dmp \
-    -n /data/kaijudb/names.dmp \
-    -i /data/${i} 
-    -r genus \ 
-    -m 1 \
-    -o /data/${i}.kaiju_out_krona.1percenttotal.summary
+    docker run -v /home/ubuntu/data:/data quay.io/biocontainers/kaiju:1.6.1--pl5.22.0_0 \
+        kaijuReport 
+        -v \
+        -t /data/kaijudb/nodes.dmp \
+        -n /data/kaijudb/names.dmp \
+        -i /data/${i} \
+        -r genus \
+        -m 1 \
+        -o /data/${i}.kaiju_out_krona.1percenttotal.summary
 done
 
 # Now for comparison, let's take the genera that comprise at least 1 percent of all of the classified reads:
 
 for i in *trim{"2","30"}.out
 do
-        docker run -v /home/ubuntu/data:/data quay.io/biocontainers/kaiju:1.5.0--pl5.22.0_0 kaijuReport \
-    -v \
-    -t \
-        /data/kaijudb/nodes.dmp \
-    -n /data/kaijudb/names.dmp \
-    -i /data/${i} 
-    -r genus \
-    -m 1 \
-    -u \
-    -o /data/${i}.kaiju_out_krona.1percentclassified.summary
+    docker run -v /home/ubuntu/data:/data quay.io/biocontainers/kaiju:1.6.1--pl5.22.0_0 \
+        kaijuReport \
+        -v \
+        -t /data/kaijudb/nodes.dmp \
+        -n /data/kaijudb/names.dmp \
+        -i /data/${i} \
+        -r genus \
+        -m 1 \
+        -u \
+        -o /data/${i}.kaiju_out_krona.1percentclassified.summary
 done
 
