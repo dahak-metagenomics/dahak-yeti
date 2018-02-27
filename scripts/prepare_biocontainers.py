728f0cfb5587507abab209716631efe1b4d8c65a
Charles Reid <charlesreid1@gmail.com>
2018-02-27 11:11:22 -0800

    Bump kaiju version number

--- scripts/prepare_biocontainers.py
+++ scripts/prepare_biocontainers.py
@@ -4,7 +4,7 @@ import subprocess
 
 quayurls = [ "quay.io/biocontainers/sourmash:2.0.0a3--py36_0",
              "quay.io/biocontainers/krona:2.7--pl5.22.0_1",
-             "quay.io/biocontainers/kaiju:1.5.0--pl5.22.0_0"]
+             "quay.io/biocontainers/kaiju:1.6.1--pl5.22.0_0"]
 
 pwd = os.getcwd()
 for quayurl in quayurls:
