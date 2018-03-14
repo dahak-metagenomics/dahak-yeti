394954687afe8466f405ae22ffef0a65bb1dd328
Charles Reid <charlesreid1@gmail.com>
2018-03-13 18:07:18 -0700

    Adding initial version of user init scripts, mainly prebootstrap and install script

---
+++ user_init/user_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# initialize all the dotfiles stuff for user 
+set -x
+
+DOTFILES="$HOME/dotfiles"
+git clone https://github.com/charlesreid1/dahak-yeti $DOTFILES
+
+./pre_bootstrap.sh
+(
+cd ../
+./bootstrap.sh -f
+./gen_ssh_keys.sh
+)
+./install_pyenv.py
+./install_snakemake.py
+./install_singularity.py
+
