6f3f5c10813243c28574ed950d0edf2a3794644c
Charles Reid <charlesreid1@gmail.com>
2018-03-28 15:08:46 -0700

    add -y flag to ssh key generation

--- gen_ssh_keys.sh
+++ gen_ssh_keys.sh
@@ -5,7 +5,7 @@
 # this can be run by the root user
 # or by any regular user
 
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+ssh-keygen -y -f $HOME/.ssh/id_rsa -t rsa -N ''
 chmod 700 $HOME/.ssh
 touch $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/authorized_keys
