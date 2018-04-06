39505131bc1314ce7af39d7af2b1ae7b91d7319c
Charles Reid <charlesreid1@gmail.com>
2018-04-06 12:57:26 -0700

    add conda install script

--- tasks_user/install_conda.py
+++ tasks_user/install_conda.py
@@ -3,7 +3,6 @@ import subprocess
 
 print("")
 print("Installing conda...")
-mc = subprocess.Popen(["pyenv","install","miniconda3-4.3.30"], stdout=subprocess.PIPE)
-print(mc.stdout().read())
+mc = subprocess.call(["pyenv","install","miniconda3-4.3.30"])
 print("Done installing conda.")
 print("")
