48ec61f350051d820dc926032790a852fc4c5e3b
Chaz Reid <charlesreid1@Gmail.com>
2018-04-17 14:31:33 -0700

    Create prepare_vim.sh

---
+++ tasks_user/prepare_vim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install snakemake syntax highlighting for vim
+wget https://bitbucket.org/snakemake/snakemake/raw/master/misc/vim/syntax/snakemake.vim -O ~/.vim/syntax/snakemake.vim
