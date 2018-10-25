e47254a4ba95ae135b47a53af1b85a4ed3a254a6
Charles Reid <charlesreid1@gmail.com>
2018-10-25 13:49:46 -0700

    update sudo init to install docker compose

--- tasks_sudo/get_docker.sh
+++ tasks_sudo/get_docker.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+    echo ""
+    echo ""
+    echo "This script should be run as root."
+    echo ""
+    echo ""
+    exit 1;
+fi
+
 bash <( curl https://get.docker.com/ )
 
 usermod -aG docker ubuntu
